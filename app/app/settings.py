6f11bc4fa01019fe1f138488b46e6054a71e3248
tarekgabarin <tarekgabarin1992@gmail.com>
2023-02-28 16:43:25 -0500

    try again

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,13 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-def get_secret(key, default):
-    value = os.getenv(key, default)
-    if os.path.isfile(value):
-        with open(value) as f:
-            return f.read()
-    return value
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("SECRET_KEY", "")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
