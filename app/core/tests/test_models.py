6af878b9bfa55b67d480d8f1ac342032acfcc4e0
tarekgabarin <tarekgabarin1992@gmail.com>
2023-03-01 23:03:05 -0500

    added tests and method for creating new user

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,40 @@
+"""
+Testing for models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test Models"""
+
+    def test_if_create_user_with_email_successful(self):
+        """Test if can successfully create a user with an email"""
+
+        test_password = "testing123"
+        test_email = "example@test.com"
+        
+        test_user = get_user_model().objects.create_user(
+            email=test_email,
+            password=test_password
+        )
+
+        self.assertEqual(test_user.email, test_email)
+        self.assertTrue(test_user.check_password(test_password))
+
+    def test_if_created_user_email_normalized(self):
+        """Test if the email of a newly-created user is normalized"""
+        arr_of_test_sample_emails = [
+            ["example@TEST.com", "example@test.com"],
+            ['myemail@exAMPLE.com', 'myemail@example.com'],
+            ['Example2@tEST.com', 'Example2@test.com']
+        ]
+
+        for inputted_email, expected in arr_of_test_sample_emails:
+            created_user = get_user_model().objects.create_user(inputted_email, "testing123")
+            self.assertEqual(created_user.email, expected)
+
+    def test_if_creating_user_without_email_throws_error(self):
+        """Test to see if creating a user without an email provided throws a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'testing123')
