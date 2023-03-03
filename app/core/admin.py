79afeedba9e54cc556d1a82e5069e1cb73b9527e
tarekgabarin <tarekgabarin1992@gmail.com>
2023-03-03 16:58:59 -0500

    create and modify users in django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin # noqa
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            ('Permissions'),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser"
+                )
+            }
+        )
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            "classes": ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
